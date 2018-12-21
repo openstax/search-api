d38c8e3ad9449a0ccc9eae8a58678037ba4c7e40
JP Slavinsky <jpslav@gmail.com>
2018-12-21 15:45:49 -0700

    using newer install_app.sh approach

---
+++ install_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to run on the deployed server when the code has been
+# updated (or on first deployment)
+
+ruby_version=`cat .ruby-version`
+echo Installing Ruby $ruby_version
+source /home/ubuntu/rbenv-init && rbenv install -s $ruby_version
+
+echo Installing bundler
+gem install --conservative bundler
+
+echo Installing gems
+bundle install --without development test
+
+echo Installing JS packages
+yarn install
+
+echo Done!
