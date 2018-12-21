347e5bce508f62555bca497c4f1b3475d3fffe11
JP Slavinsky <jpslav@gmail.com>
2018-12-21 10:58:56 -0700

    search prototype

--- Gemfile
+++ Gemfile
@@ -17,6 +17,11 @@ gem 'openstax_healthcheck'
 # For installing secrets on deploy
 gem "aws-sdk-ssm"
 
+# Versioned API tools
+gem "versionist"
+
+gem "elasticsearch", '~> 6.1.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+
+    api_version(
+      module: 'V0',
+      path: { value: 'v0' },
+      defaults: { format: :json },
+      default: true
+    ) do
+
+      get :search, to: 'search#search'
+
+      get :temp_build_index, to: 'search#temp_build_index'
+
+    end
+
+  end
+
 end
