84a6197670871a99ccfa8a817c27903f84fd453e
JP Slavinsky <jpslav@gmail.com>
2019-01-05 20:18:51 -0700

    use temporary credentials during specs for added security

--- spec/support/temp_aws_env.rb
+++ spec/support/temp_aws_env.rb
@@ -4,7 +4,11 @@ class TempAwsEnv
 
   include VcrHelperMethods
 
+  @@have_switched_to_temporary_credentials = false
+
   def self.make
+    switch_to_temporary_credentials
+
     begin
       env = new()
       yield(env)
@@ -32,5 +36,47 @@ class TempAwsEnv
 
   protected
 
+  def self.switch_to_temporary_credentials
+    # In the VCR configs, we have calls that mask AWS credentials in recorded AWS HTTP
+    # interactions.  Just in case that ever doesn't work, this function changes the
+    # credentials that are used over to temporary credentials (via an AWS STS call).
+    # The temporary credentials expire after 15 minutes.
+
+    # We only need to switch to temporary credentials if we're recording a cassette; if we
+    # aren't, the non-temporary credentials aren't going to be written to a cassette.  Also,
+    # we don't want to run these HTTP requests if we are just doing playback because below we
+    # exempt them from being recorded at all (for security); if we make requests that aren't
+    # present in the cassette during playback, VCR chokes.
+
+    return if !VCR.current_cassette.try(:recording?)
+
+    # We only need to switch once; after we switch the env vars are set
+
+    return if @@have_switched_to_temporary_credentials
+
+    begin
+      # Disable VCR while we get the new credentials, because if we wrote the non-temp
+      # credentials to a cassette, that'd kind of defeat the purpose of getting temp
+      # ones
+
+      ENV['VCR_IGNORE_REQUESTS_TEMPORARILY'] = 'true'
+
+      sts_client = Aws::STS::Client.new(region: "us-east-1")
+      resp = sts_client.get_session_token({duration_seconds:900})
+      @@temporary_credentials = resp.credentials
+
+      ENV['AWS_ACCESS_KEY_ID'] = resp.credentials.access_key_id
+      ENV['AWS_SECRET_ACCESS_KEY'] = resp.credentials.secret_access_key
+      ENV['AWS_SESSION_TOKEN'] = resp.credentials.session_token
+
+      filter_env_var('AWS_ACCESS_KEY_ID')
+      filter_env_var('AWS_SECRET_ACCESS_KEY')
+      filter_env_var('AWS_SESSION_TOKEN')
+
+      @@have_switched_to_temporary_credentials = true
+    ensure
+      ENV.delete('VCR_IGNORE_REQUESTS_TEMPORARILY')
+    end
+  end
 
 end
--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -14,13 +14,31 @@ VCR.configure do |c|
   # in Travis build logs that are too large and cause a Travis error
   # c.debug_logger = $stderr
 
+  # We try to use temporary AWS credentials in specs (see TempAwsEnv), but just in
+  # case we don't, we make sure to filter non-temp credentials that may be set
+  # in the env vars when the spec run starts.
+
   filter_env_var('AWS_ACCESS_KEY_ID')
   filter_env_var('AWS_SECRET_ACCESS_KEY')
+  filter_env_var('AWS_SESSION_TOKEN')
+
+  # Probably not super critical to filter out the AWS Signature, because I believe the
+  # signature includes header elements as well as the request body (so any change would
+  # be found out), but for safety's sake we do it anyway.
 
   c.filter_sensitive_data('<SignatureValue>') do |interaction|
     (interaction.request.headers["Authorization"] || []).first.match(/Signature=([a-f0-9]+)/)
     $1
   end
+
+  # This block lets us skip writing a few requests to a cassette.  Note those requests
+  # must be skipped when a cassette isn't being recorded, otherwise VCR won't know what
+  # to do with those requests.
+
+  c.ignore_request do |request|
+    'true' == ENV['VCR_IGNORE_REQUESTS_TEMPORARILY']
+  end
+
 end
 
 VCR_OPTS = {
