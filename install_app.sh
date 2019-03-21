dc289db33790741cdf4f7f8f5960bf5db8e5eb5a
JP Slavinsky <jpslav@gmail.com>
2019-03-20 19:40:11 -0600

    fail fast for install script

--- install_app.sh
+++ install_app.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Exit with non-zero status if a simple command fails, even with piping
+# https://stackoverflow.com/a/4346420/1664216
+
+set -e
+set -o pipefail
+
 # Script to run on the deployed server when the code has been
 # updated (or on first deployment)
 
